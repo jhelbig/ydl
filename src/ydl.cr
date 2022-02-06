393dcf916ef71a3c49c7d7b935b3400c1f794364
jhelbig <jhelbig@users.noreply.github.com>
2022-02-06 15:07:24 -0600

    Update ydl.cr

--- src/ydl.cr
+++ src/ydl.cr
@@ -83,6 +83,34 @@ module Ydl
 
       File.join(dir, download_name(format))
     end
+
+    def download_and_mux(formats : Array(Ydl::Format))
+      formats.each{|format|
+        name = download_name(format).gsub(/\.mp\d$/) { "" }
+        dir = File.expand_path(File.join(ENV.fetch("YDL_PATH", "~/ydl_downloads")))
+        Dir.mkdir_p(dir)
+        Dir.cd(dir)
+
+        ydl_args = [
+          "-f", format.id,
+          "-o", %<#{name}.%(ext)s>,
+          @url,
+        ]
+
+        if format.resolution == "Audio"
+          ydl_args << "--audio-format"
+          ydl_args << "mp3"
+          ydl_args << "-x"
+        else
+          ydl_args << "--recode-video"
+          ydl_args << "mp4"
+        end
+
+        status = Process.run("youtube-dl", ydl_args)
+
+        File.join(dir, download_name(format))
+      }
+    end
   end
 
   struct Format
@@ -102,7 +130,7 @@ module Ydl
       @extension = f["ext"].as_s
       @quality_grade = f["quality"].as_i
 
-      @resolution = "Not Applicable"
+      @resolution = "Audio"
 
       begin
         @resolution = %<#{f["width"].as_i}x#{f["height"].as_i}>
