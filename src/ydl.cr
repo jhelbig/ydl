8083b45a854b5e77adf3b12055213ec59cb73a9a
jhelbig <jhelbig@users.noreply.github.com>
2022-02-12 00:01:48 -0600

    Update ydl.cr

--- src/ydl.cr
+++ src/ydl.cr
@@ -40,9 +40,9 @@ module Ydl
 
     def download_name(format : Ydl::Format)
       if format.resolution == "Audio"
-        %<#{@title} - (#{format.quality}hz).mp3>
+        %<'#{@title.scrub("")} - (#{format.quality}hz).mp3'>
       else
-        %<#{@title} - (#{format.quality}p).mp4>
+        %<'#{@title.scrub("")} - (#{format.quality}p).mp4'>
       end
     end
 
